12a924c91b926a316d47e19d2da83c6aaace77f9
lmissher <fyclmiss@gmail.com>
2019-11-21 12:10:42 +0800

    softmax modle

--- d2lzh_pytorch/utils.py
+++ d2lzh_pytorch/utils.py
@@ -67,6 +67,8 @@ def sgd(params, lr, batch_size):
     for param in params:
         param.data -= lr * param.grad / batch_size # 注意这里更改param时用的param.data
 
+
+#################################3.5########################
 # 将数值转换为文本标签
 def get_fashion_mnist_labels(labels):
     text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandle', 'shirt', 'sneaker', 'bag', 'ankle boot']
@@ -82,3 +84,64 @@ def show_fashion_mnist(images, labels):
         f.axes.get_xaxis().set_visible(False)
         f.axes.get_yaxis().set_visible(False)
     plt.show()
+
+# 加载数据集并返回iter
+def load_data_fashion_mnist(batch_size,root='~/DL/Datasets/FashionMNIST'):
+    transform = transforms.ToTensor()
+    mnist_train = torchvision.datasets.FashionMNIST(
+    root=root, train=True, download=True, transform=transforms.ToTensor())
+    mnist_test = torchvision.datasets.FashionMNIST(
+    root=root, train=False, download=True, transform=transforms.ToTensor())
+    if sys.platform.startswith('win'):
+        num_workers = 0
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train,batch_size
+                                        =batch_size,shuffle=True,num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test,batch_size
+                                        =batch_size,shuffle=False,num_workers=num_workers)
+    return train_iter, test_iter
+
+# softmax函数
+def soft_max(X):
+    X_exp = X.exp()
+    div = X_exp.sum(dim=1,keepdim=True)
+    return X_exp/div
+
+
+# 评价模型net在数据集iter上的准确率
+def evaluate_accuracy(data_iter,net):
+    acc_sum , n = 0.0,0
+    for X,y in data_iter:
+        acc_sum += (net(X).argmax(dim=1)==y).float().sum().item()
+        n += y.shape[0]
+    return acc_sum/n
+
+#softmax模型训练
+def train_ch3(net, train_iter, test_iter, loss, epochs, batch_size, params=None, lr=None, optimizer=None):
+    for epoch in range(epochs):
+        train_l_sum, train_acc_sum, n=0.0,0.0,0
+        for X,y in train_iter:
+            y_hat=net(X)
+            l=loss(y_hat,y).sum()
+            
+            # 梯度清零
+            if optimizer is not None:
+               optimizer.zero_grad()
+            elif params is not None and params[0].grad is not None:
+               for param in params:
+                   param.grad.data.zero_()
+
+            l.backward()
+            if optimizer is None:
+               sgd(params, lr, batch_size)
+            else:
+               optimizer.step()
+
+            train_l_sum += l.item()
+            train_acc_sum += (y_hat.argmax(dim=1)==y).sum().item()
+            n += y.shape[0]
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f' %
+               (epoch+1,train_l_sum/n,train_acc_sum/n,test_acc))
+
